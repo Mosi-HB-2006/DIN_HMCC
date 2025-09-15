453cda990a2a787f8dc5f91d79ad0bfb9da575fc
Usuario <Usuario@DESKTOP-CCIMVVO>
2025-09-15 09:42:31 +0200

    Added classes and stuff (for real now)

--- Reto1DIN/src/userInterface/FXMLDocumentController.java
+++ Reto1DIN/src/userInterface/FXMLDocumentController.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package reto1din;
+package userInterface;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,24 +7,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author Mosi
- */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        
+    }
 }
