611ee882d57c5edf8be5b0b3d3b03e6be9e4f1bd
Jago128 <>
2025-09-22 08:32:40 +0200

    User's ready

--- Reto1DIN/src/model/User.java
+++ Reto1DIN/src/model/User.java
@@ -1,7 +1,73 @@
 package model;
 
 import java.io.*;
+import java.time.*;
 
 public class User implements Serializable {
-    
+    private String username;
+    private String password;
+    private LocalDate birthDate;
+    private String desc;
+    private Gender gender;
+
+    public User() {
+        this.username = "";
+        this.password = "";
+        this.birthDate = LocalDate.now();
+        this.desc = "";
+        this.gender = Gender.OTHER;
+    }
+
+    public User(String username, String password, LocalDate birthDate, String desc, Gender gender) {
+        this.username = username;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.desc = desc;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", password=" + password + ", birthDate=" + birthDate + ", desc=" + desc + ", gender=" + gender + '}';
+    }
 }
