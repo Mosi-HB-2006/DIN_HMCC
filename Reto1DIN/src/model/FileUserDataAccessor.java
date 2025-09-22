e7fa2b08b06ae1c5ca43de0165bc6e9533c61220
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2025-09-22 08:56:48 +0200

    Adding access data to fileDataAccessor

--- Reto1DIN/src/model/FileUserDataAccessor.java
+++ Reto1DIN/src/model/FileUserDataAccessor.java
@@ -2,18 +2,37 @@ package model;
 
 import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class FileUserDataAccessor implements DataAccessible {
 
-    public User accessDataFile(File fich) {
+    @Override
+    public User accessDataFile(String username, String password) {
+        File fich = new File("usuarios.obj");
         boolean endFile = false;
-        
-        try{
-        }catch (EOFException e){
-            endFile = true;
+        User finalUser = null;
+
+        if (fich.exists()) {
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fich));
+                try {
+                    do {
+                        User user = (User) ois.readObject();
+
+                        if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                            finalUser = user;
+                            endFile = true;
+                        }
+                    } while (!endFile);
+                } catch (EOFException e) {
+                    endFile = true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        
-        
-        return null;
+
+        return finalUser;
     }
 }
