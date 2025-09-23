f422dfda30f08d68d79f772178a466bb0ae3ebed
Usuario <Usuario@DESKTOP-CCIMVVO>
2025-09-23 09:33:27 +0200

    welp

--- Reto1DIN/src/userInterface/Main.java
+++ Reto1DIN/src/userInterface/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/userInterface/LoginWindow.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
